37769d14c10bef5cdf9fd0173804bd6a75ab93fb
Anderson Gonzalez <profesional.ander@gmail.com>
2022-05-26 17:15:01 -0500

    Test

---
+++ Introduccion/Ejercicio 1 - Print .py
@@ -0,0 +1,6 @@
+print(123)
+print(-21)
+print(3.14)
+print(999.999)
+print(100)
+print(777)
---
+++ Introduccion/Ejercicio 6 - Inputs.py
@@ -0,0 +1,9 @@
+# Completa el ejercicio
+nombre = input("Ingrese el nombre: ")
+apellido = input("Ingrese el apellido: ")
+
+edad = int(input("Ingrese la edad: "))
+
+numero_magico = float(input("Ingrese un flotante: "))
+
+cadena_final = nombre + " " + apellido + ": Tu número de la suerte es el " + str(edad * numero_magico)

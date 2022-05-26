cd6560f6533e20da5f9177854d3da1bcdcb43af2
Anderson Gonzalez <profesional.ander@gmail.com>
2022-05-26 17:26:54 -0500

    archivos repetidos

--- Ejercicio 1 - Print .py
+++
@@ -1,6 +0,0 @@
-print(123)
-print(-21)
-print(3.14)
-print(999.999)
-print(100)
-print(777)
--- Ejercicio 2 - Variables.py
+++
@@ -1,7 +0,0 @@
-# Variables del ejercicio
-nota_1 = 10
-nota_2 = 6
-
-# Calcula correctamente la nota_final
-nota_final = (nota_1 * 0.4)+(nota_2 * 0.6)
-print(nota_final)

010940c5b0b5a5b194481b9a6b5f401bb77848d5
Anderson Gonzalez <profesional.ander@gmail.com>
2022-05-26 19:48:24 -0500

    Python

---
+++ Operadores y Expresiones/Ejercicio 7 - Operadores relacionales.py
@@ -0,0 +1,4 @@
+#from evaluate import expresiones
+
+# completa el ejercicio
+resultados = [False, True, True, True, True]
---
+++ Operadores y Expresiones/Ejercicio 8 - Expresiones anidadas.py
@@ -0,0 +1,4 @@
+#from evaluate import edad, nombre
+
+# completa el ejercicio
+# expresion = (nombre != "****" and len(nombre) >= 3 and len(nombre) < 10) and(edad > 10 and edad < 18)
